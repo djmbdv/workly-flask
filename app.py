3ee898cfe96acaeb4ed258505fbd2f6175e777ea
David José Márquez Batiz <djmbdv@gmail.com>
2021-06-10 18:19:17 -0400

    rest test

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from flask import Flask
 from datetime import datetime
+from flask_restful import Resource, Api
 app = Flask(__name__)
+api = Api(app)
+
+class Saludo(Resource):
+    def get(self):
+        return {'hello':"world"}
+
+
+api.add_resource(Saludo, "/api")
 
 @app.route('/')
 def homepage():
-    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
+    the_time = 234
 
     return """
     <h1>Hello heroku</h1>
@@ -12,6 +21,7 @@ def homepage():
 
     <img src="http://loremflickr.com/600/400">
     """.format(time=the_time)
+
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
