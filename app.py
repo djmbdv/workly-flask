e71eeae3b2d56e22ab7a05bde32201f39f363bfd
David José Márquez Batiz <djmbdv@gmail.com>
2021-06-25 18:44:18 -0400

    cors

--- app.py
+++ app.py
@@ -3,13 +3,13 @@ from flask_restful import reqparse, Resource, Api
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt import JWT, jwt_required
 from flask_marshmallow import Marshmallow
-
+from flask_cors import CORS
 app = Flask(__name__)
 api = Api(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://xdjdzwflubsbjj:0d4c6657d92f66fb060d04a7a2ea3e0a5ba72cec8ccc2dae42eabea0de814b8b@ec2-52-23-40-80.compute-1.amazonaws.com:5432/d6l3uvcb2m9r53" # 'sqlite:///test.db'
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
-
+CORS(app)
 
 from models import *
 
