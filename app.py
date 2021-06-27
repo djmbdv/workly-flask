d2b60ff790c242ed6980b6c7182bd5a020c5189b
David José Márquez Batiz <djmbdv@gmail.com>
2021-06-27 09:07:10 -0400

    tokens JWT

--- app.py
+++ app.py
@@ -4,10 +4,14 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_jwt import JWT, jwt_required
 from flask_marshmallow import Marshmallow
 from flask_cors import CORS
+from werkzeug.security import generate_password_hash, check_password_hash
 app = Flask(__name__)
 api = Api(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://xdjdzwflubsbjj:0d4c6657d92f66fb060d04a7a2ea3e0a5ba72cec8ccc2dae42eabea0de814b8b@ec2-52-23-40-80.compute-1.amazonaws.com:5432/d6l3uvcb2m9r53" # 'sqlite:///test.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False;
+app.config['SECRET_KEY'] = 'super-secret'
+
+
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 CORS(app)
@@ -15,8 +19,23 @@ CORS(app)
 from models import *
 
 
+def authenticate(usern, password):
+    user = User.query.filter_by(username = usern).first()
+    if user and check_password_hash(user.password, password):
+        return user
+
+def identity(payload):
+    user_id = payload['identity']
+    print(user_id)
+    return User.query.filter_by(id = user_id).first()
+
+
+jwt = JWT(app, authenticate, identity)
+ 
 parser = reqparse.RequestParser()
+
 class Homepage(Resource):
+    @jwt_required()
     def get(self):
         return {'message':'hola mundo'}    
 
@@ -41,6 +60,7 @@ class UsersApi(Resource):
         arg =  parser.parse_args()
         parser.add_argument('username', type=str, required=True)
         parser.add_argument('email', type=str, required=True)
+
         return arg
 
 api.add_resource(Homepage, "/")
--- migrate.py
+++ migrate.py
@@ -1,7 +1,8 @@
 from app import db
 from models import User
-admin = User(username='admin',password="holamundo" ,email='admin@example.com')
-guest = User(username='guest', password="test" ,email='guest@example.com')
+from werkzeug.security import generate_password_hash
+admin = User(username='admin',password=generate_password_hash("holamundo") ,email='admin@example.com')
+guest = User(username='guest', password=generate_password_hash("test") ,email='guest@example.com')
 db.drop_all()
 db.create_all()
 db.session.add(admin)
